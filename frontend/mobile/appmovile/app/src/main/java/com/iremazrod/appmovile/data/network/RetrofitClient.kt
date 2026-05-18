65a317999ddb1c988a2f3f9b26facf2675ba7020
MssMunroe <irenemazarodriguez@gmail.com>
2026-05-18 16:43:41 +0200

    CSS Responsive para moviles

--- frontend/mobile/appmovile/app/src/main/java/com/iremazrod/appmovile/data/network/RetrofitClient.kt
+++ frontend/mobile/appmovile/app/src/main/java/com/iremazrod/appmovile/data/network/RetrofitClient.kt
@@ -6,21 +6,20 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    // 10.0.2.2 es la IP especial para que el emulador de Android vea el localhost de tu PC (Flask)
-    private const val BASE_URL = "http://10.0.2.2:5000/"
+    private const val BASE_URL = "https://bosquea-backend.onrender.com/"
 
-    // Configuramos un cliente OkHttp para controlar tiempos de espera y logs
+    // controlar tiempos de espera y logs
     private val okHttpClient = OkHttpClient.Builder()
-        .connectTimeout(30, TimeUnit.SECONDS) // Tiempo máximo para conectar
-        .readTimeout(30, TimeUnit.SECONDS)    // Tiempo máximo para recibir datos
-        .writeTimeout(30, TimeUnit.SECONDS)   // Tiempo máximo para enviar datos (útil para subir fotos)
+        .connectTimeout(90, TimeUnit.SECONDS)
+        .readTimeout(90, TimeUnit.SECONDS)
+        .writeTimeout(90, TimeUnit.SECONDS)
         .build()
 
     val instance: BosqueaApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(okHttpClient) // Añadimos el cliente configurado
-            .addConverterFactory(GsonConverterFactory.create()) // Convierte JSON a tus Data Classes
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(BosqueaApiService::class.java)
     }
